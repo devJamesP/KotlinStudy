637a23543ef53ddeaf82a4fea113b9e44ccbf7ce
devJamesP <znzldh1234@gmail.com>
2021-05-15 12:38:00 +0900

    07-2 일부 수정ls

--- KotlinStudy/src/chapter_07/chapter_07_2/07_2.5. 실드 클래스 작성하기.kt
+++ KotlinStudy/src/chapter_07/chapter_07_2/07_2.5. 실드 클래스 작성하기.kt
@@ -12,11 +12,11 @@ class Inside: Result.Success("Status")
 // 실드 클래스를 선언하는 두 번쨰 방법
 sealed class Result2
 
-open class Success2(val message: String): Result()
-class Error2(val code: Int,val message: String): Result()
+open class Success2(val message: String): Result2()
+class Error2(val code: Int,val message: String): Result2()
 
-class Status2: Result()
-class Insede2: Success2("Status")
+class Status2: Result2()
+class Inside2: Success2("Status")
 
 fun main() {
     // Success에 대한 객체 생성
@@ -30,9 +30,5 @@ fun eval(result: Result): String = when(result) {
     is Status -> "in progress"
     is Result.Success -> result.message
     is Result.Error -> result.message
-
-    is Status2 -> "in progress"
-    is Success2 -> result.message
-    is Error2 -> result.message
     // 모든 조건을 가지므로 else가 필요 없음.
 }
